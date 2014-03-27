1e527a338bc6f0134470a9c1c0d05050d3e6e73e
donnvalencia <donnvalencia@gmail.com>
2014-03-26 20:38:22 -0400

    Work on registration form
    Add inputs to registration form.  Stub out controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('RegistrationCtrl', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
